16cc2efadc81f97bc6ce6e434ed82dadd8a8abe9
amadou <amadou.barro.etu@univ-lille.fr>
2022-04-20 01:27:00 +0200

    rendu livrable 3

---
+++ src/game/Quest.java
@@ -0,0 +1,41 @@
+package game;
+import grille.Board;
+import grille.Cell;
+import java.util.*;
+import character.*;
+import character.Character;
+
+public class Quest {
+	
+	private Board board;
+	private Cell position;
+	public final int goldvalue;
+	
+	public Quest(Board board) {
+		this.board = board;
+		this.position = board.getCell(0,0);
+		this.goldvalue = 50;
+	}
+	
+	public void setPosition() {
+		Random rand = new Random();
+		int x = rand.nextInt(this.board.getrows());
+		int y = rand.nextInt(this.board.getcols());
+		this.position = this.board.getCell(x, y);
+	}
+	
+	public Cell getPosition() {
+		return this.position;
+	}
+	
+	public boolean getQuest(Character c) {
+		if(c instanceof Heros && ((Heros) c).getGoldValue() == goldvalue && ((Heros)c).getPosition().equals(this.getPosition())) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	
+}
